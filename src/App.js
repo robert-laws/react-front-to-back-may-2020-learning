bf960d76f1a5f72fc8f98917dfb5c55904e1682d
Robert Laws <roblaws@me.com>
2020-05-23 12:18:45 +0300

    add antd support

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
-
+import { Button, message } from 'antd';
 import './App.scss';
+import './App.less';
 
 function App() {
+  const handleClick = () => {
+    message.info('Welcome to the App');
+  }
+
   return (
     <div className="App">
-      
+      <h1>App</h1>
+      <Button type='primary' onClick={handleClick}>Click Here!!</Button>
     </div>
   );
 }
